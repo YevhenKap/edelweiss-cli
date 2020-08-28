909f91523b51893dc5cc66ae053fc5bf46520b4d
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2020-08-28 23:05:47 +0300

    Returns back to default css modules configurations

--- templates/typescript/@types/file_modules.d.ts
+++ templates/typescript/@types/file_modules.d.ts
@@ -25,93 +25,22 @@ declare module '*.svg' {
 }
 
 // Styles
-declare module '*.m.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
 
-declare module '*.m.pcss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.pcss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.pcss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.m.postcss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.postcss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.postcss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.m.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
 declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
 
-declare module '*.m.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
 declare module '*.module.sass' {
   const classes: { [key: string]: string };
   export default classes;
 }
 
-declare module '*.m.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
 declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.m.styl' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.mod.styl' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.styl' {
   const classes: { [key: string]: string };
   export default classes;
 }
